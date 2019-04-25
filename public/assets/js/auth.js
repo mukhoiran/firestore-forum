8524e35af7d5b565e2098f8a04fda23118188129
mukhoiran <mkhoir.anam@gmail.com>
2019-04-25 15:36:19 +0700

    login gmail

---
+++ public/assets/js/auth.js
@@ -0,0 +1,40 @@
+var auth = firebase.auth();
+
+function login(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+    console.log('login success');
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    console.log('failed login');
+  });
+}
+
+function logout(){
+  auth.signOut().then(function(){
+    console.log('logout success!');
+  }).catch(function(err){
+    console.log('logout failed');
+  })
+}
+
+auth.onAuthStateChanged(function(user){
+  if(user){
+    console.log('login...')
+  }else{
+    console.log('logout...')
+  }
+})
