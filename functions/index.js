5a89dd9db333b20bbb5bc9e315f90f01f4191950
mukhoiran <mkhoir.anam@gmail.com>
2019-04-26 10:33:53 +0700

    setup firebase admin nodejs

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,36 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const express = require('express');
 const cons = require('consolidate');
 const app = express();
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://forum-4d67d.firebaseio.com"
+});
+
+const db = admin.firestore();
 
 app.engine('hbs', cons.handlebars);
 app.set('view engine','hbs');
 app.set('views','./views');
 
 app.get('/forum', function(req, res){
-  res.render('forum');
+  var forums = []
+
+  db.collection('forums').orderBy('updated_at','desc').get()
+    .then(snapshoot => {
+      snapshoot.forEach(doc => {
+        forums.push(doc.data())
+      })
+
+      res.render('forum', {forums: forums});
+
+    }).catch(err => {
+      console.log('failed load data')
+      console.log(err)
+    })
 })
 exports.app = functions.https.onRequest(app);
